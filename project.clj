8ad05ee3db2a4a19a5cbdae094bf0443fc79c9c9
Reno Reckling <reno.reckling@mayflower.de>
2014-07-27 04:56:15 +0200

    initial with put and get working

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-hbase "0.1.0-SNAPSHOT"
+  :description "hbase driver for clojure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.apache.hbase/hbase-client "0.98.3-hadoop2"]
+                 [org.apache.hbase/hbase-common "0.98.3-hadoop2"]])
---
+++ src/clj_hbase/core.clj
@@ -0,0 +1,104 @@
+(ns clj-hbase.core
+  (:refer-clojure :rename {get map-get})
+  (:import [org.apache.hadoop.hbase.client HConnectionManager Get Put Result]
+           [org.apache.hadoop.hbase HBaseConfiguration]
+           [org.apache.hadoop.fs Path]))
+
+(defprotocol HBaseProto
+  (get [this table-name row spec result-mapping])
+  (put [this table-name row values]))
+
+(defn create-config [config-map]
+  (let [config (HBaseConfiguration/create)]
+    (doall (for [[k v] config-map]
+             (cond
+              (= k :resources) (doall (map #(.addResource config (Path. %)) v))
+              (string? v) (.set config (name k) v)
+              (instance? Boolean v) (.setBoolean config (name k) v)
+              (instance? Double v) (.setDouble config (name k) v)
+              (instance? Long v) (.setLong config (name k) v)
+              (instance? Float v) (.setFloat config (name k) v)
+              (instance? Integer v) (.setInteger config (name k) v)
+              (seq? v) (.setStrings config (name k) v))))
+    config))
+
+(defn create-connection [^HBaseConfiguration config] (HConnectionManager/createConnection config))
+
+(defn create-table [connection table-name] (.getTable connection table-name))
+
+(defn ->bytes [item] (cond (string? item) (.getBytes item)
+                           (instance? (type (byte-array 0)) item) item
+                           (keyword? item) (.getBytes (name item))
+                           :else (throw (Exception. (str "Unable to put type " (class item))))))
+
+(defn create-get [row spec]
+  (let [g (Get. (->bytes row))]
+    (when-let [family (:family spec)]
+      (if-let [column (:column spec)]
+        (.addColumn g (->bytes family) (->bytes column))
+        (.addFamily g (->bytes family))))
+    g))
+
+(defn create-put [row values]
+  (let [p (Put. (->bytes row))]
+    (doall
+     (for [family-key (keys values)]
+       (let [family (map-get values family-key)]
+         (if (not (map? family))
+           (.add
+            p
+            (->bytes family-key)
+            (->bytes "")
+            (->bytes family))
+           (doall
+            (for [column-key (keys family)]
+              (.add
+               p
+               (->bytes family-key)
+               (->bytes column-key)
+               (->bytes (map-get family column-key)))))))))
+    p))
+
+(defn unwrap-result [^Result result result-mapping]
+  (let [family-mapper (map-get result-mapping :family #(String. %))
+        timestamp-mapper (map-get result-mapping :timestamp #(java.util.Date. %))
+        column-mapper (map-get result-mapping :column {})
+        default-column-mapper identity
+        value-mapper (map-get result-mapping :value {})
+        default-value-mapper identity
+        result-map (.getMap result)
+        value-reducer (fn [family column acc [timestamp value]]
+                        (let [mapper (get-in value-mapper [family column]
+                                             (get-in value-mapper [family :*] default-value-mapper))]
+                          (assoc acc (timestamp-mapper timestamp) (mapper value))))
+        column-reducer (fn [family acc [column values]]
+                         (let [mapper (map-get column-mapper family default-column-mapper)
+                               mapped-column (mapper column)]
+                           (assoc acc mapped-column
+                             (reduce (partial value-reducer family mapped-column) {} values))))
+        family-reducer (fn [acc [family columns]]
+                         (let [mapped-family (family-mapper family)]
+                           (assoc
+                             acc
+                             mapped-family
+                             (reduce (partial column-reducer mapped-family) {} columns))))]
+    (reduce family-reducer {} result-map)))
+
+(deftype HBase [connection]
+  HBaseProto
+  (get [this table-name row spec result-mapping]
+       (unwrap-result
+        (.get
+         (create-table connection table-name)
+         (create-get row spec))
+        result-mapping))
+  (put [this table-name row values]
+       (.put
+         (create-table connection table-name)
+         (create-put row values))))
+
+(defn create-database
+  ([] (create-database {}))
+  ([config-map] (HBase.
+                 (create-connection
+                  (create-config config-map)))))
